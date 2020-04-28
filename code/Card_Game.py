0730eef317ae50e33f8f6ebf0a3258786c182d8a
bonus <bbonnuss1@hotmail.com>
2020-04-29 04:24:17 +0700

    move song player to def main()

--- code/Card_Game.py
+++ code/Card_Game.py
@@ -68,10 +68,6 @@ class Menu():
         self.draw_bg()
         pygame.display.update()
 
-        # music theme (ต้อง load ใหม่ เพื่อเปิดเสียงแยกจาก effect)
-        pygame.mixer.music.load(self.theme_song)
-        pygame.mixer.music.set_volume(0.25)
-        pygame.mixer.music.play(loops=-1)
     
 
     def draw_bg(self):
@@ -83,7 +79,7 @@ class Main_Menu(Menu):
     def __init__(self):
         self.name = "Main Menu"
         self.background = loaded_image.main_bg
-        self.theme_song = join('resource','main_theme.wav')
+        self.theme_song = None
         self.start_button = (215,345),(600,420)
         self.exit_button = (215,450),(600,515)
 
@@ -121,11 +117,13 @@ class Main_Menu(Menu):
 
                 # start Button (to game selection)
                 if is_hit_box(mouse_pos,self.start_button[0], self.start_button[1]):
+                    print ("start")
                     if clickdown:
                         return 1
                 
                 # exit Button (exit game)
                 if is_hit_box(mouse_pos,self.exit_button[0], self.exit_button[1]):
+                    print ("exit")
                     if clickdown:
                         return -1
 
@@ -139,10 +137,12 @@ class Game_Selection_Menu(Menu):
     def __init__(self):        
         self.name = "Game Selection"
         self.background = loaded_image.selection_bg
-        self.theme_song = join('resource','main_theme.wav')
+        self.theme_song = None
         
-        self.pok_button = (215,345),(600,420)
-        self.exit_button = (215,450),(600,515)
+        self.pok_button = (200,90),(620,175)
+        self.x_button = (200,230),(620,315)
+        self.y_button = (200,360),(620,440)
+        self.back_button = (647,511),(782,578)
 
     def run(self):
         super().run()
@@ -175,15 +175,29 @@ class Game_Selection_Menu(Menu):
                 
                 # Botton ------------------------- Botton
 
-                # start Button (to game selection)
-                if is_hit_box(mouse_pos,self.start_button[0], self.start_button[1]):
+                # Pok deng Button (to game selection)
+                if is_hit_box(mouse_pos,self.pok_button[0], self.pok_button[1]):
+                    print ("pok")
                     if clickdown:
-                        return 1
+                        return 2
                 
-                # exit Button (exit game)
-                if is_hit_box(mouse_pos,self.exit_button[0], self.exit_button[1]):
+                # x Button
+                if is_hit_box(mouse_pos,self.x_button[0], self.x_button[1]):
+                    print ("x")
                     if clickdown:
-                        return -1
+                        print ("comming soon...")
+
+                # x Button
+                if is_hit_box(mouse_pos,self.y_button[0], self.y_button[1]):
+                    print ("y")
+                    if clickdown:
+                        print ("comming soon...")
+
+                # back Button (main menu)
+                if is_hit_box(mouse_pos,self.back_button[0], self.back_button[1]):
+                    print ("back")
+                    if clickdown:
+                        return 0
 
 
 class Pok_Game_Menu(Menu):
@@ -205,16 +219,27 @@ class deck():
 
 # Main ======================= Main ======================= Main
 def main():
-
+    # init 
     menu = Menu_Controller()
+
     # start at main menu
-    go = menu.main()
+    go = 0
     while go != -1:
         if go == 0:
+
+            # open main music theme (ต้อง load ใหม่ เพื่อเปิดเสียงแยกจาก effect)
+            pygame.mixer.music.load(join('resource','main_theme.wav'))
+            pygame.mixer.music.set_volume(0.25)
+            pygame.mixer.music.play(loops=-1)
+            
+
             go = menu.main()
         elif go == 1:
             go = menu.selection()
         elif go == 2:
+
+            # stop main music theme
+            pygame.mixer.music.stop
             go = menu.pok()
 
 
