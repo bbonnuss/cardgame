2b7d9a8b910346c3c12ae434d535162782afcfd5
bonus <bbonnuss1@hotmail.com>
2020-04-29 04:41:31 +0700

    add credit_menu class

--- code/Card_Game.py
+++ code/Card_Game.py
@@ -6,7 +6,8 @@ from os.path import join
 # 0 = main menu
 # 1 = game selection
 # 2 = pok game
-#
+# 32 = credit
+
 # Loader ======================= Loader ======================= Loader
 class Sound_Loader():
     def __init__(self):
@@ -39,7 +40,7 @@ class Menu_Controller():
         self.main_menu = Main_Menu()
         self.game_selection_menu = Game_Selection_Menu()
         self.pok_game_menu = Pok_Game_Menu()
-    
+        self.credit_menu = Credit_Menu()
 
     def main(self):
         return self.main_menu.run()
@@ -49,6 +50,9 @@ class Menu_Controller():
     
     def pok(self):
         return self.pok_game_menu.run()
+    
+    def credit(self):
+        return self.credit_menu.run()
 
 class Menu():   
     def __init__(self):
@@ -82,6 +86,7 @@ class Main_Menu(Menu):
         self.theme_song = None
         self.start_button = (215,345),(600,420)
         self.exit_button = (215,450),(600,515)
+        self.credit_button = (635,510),(780,577)
 
     
     def run(self):
@@ -117,21 +122,34 @@ class Main_Menu(Menu):
 
                 # start Button (to game selection)
                 if is_hit_box(mouse_pos,self.start_button[0], self.start_button[1]):
-                    print ("start")
+                    #print ("start")
                     if clickdown:
                         return 1
                 
                 # exit Button (exit game)
                 if is_hit_box(mouse_pos,self.exit_button[0], self.exit_button[1]):
-                    print ("exit")
+                    #print ("exit game")
                     if clickdown:
                         return -1
+                
+                # exit Button (exit game)
+                if is_hit_box(mouse_pos,self.exit_button[0], self.exit_button[1]):
+                    #print ("exit game")
+                    if clickdown:
+                        return 9
 
 
+class Credit_Menu(Menu):
+    def __init__(self):
+        self.name = "Credit"
+        self.background = loaded_image.bg
+        self.theme_song = None
 
+        self.next_button = (0,0),(0,0)
+        self.back_button = (0,0),(0,0)
 
-        
-
+    def draw_page1(self):
+        return None
 
 class Game_Selection_Menu(Menu):
     def __init__(self):        
@@ -159,7 +177,7 @@ class Game_Selection_Menu(Menu):
 
                 # pointer
                 mouse_pos = pygame.mouse.get_pos()
-                print (mouse_pos)
+                #print (mouse_pos)
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     clickdown = True
@@ -177,25 +195,25 @@ class Game_Selection_Menu(Menu):
 
                 # Pok deng Button (to game selection)
                 if is_hit_box(mouse_pos,self.pok_button[0], self.pok_button[1]):
-                    print ("pok")
+                    #print ("pok")
                     if clickdown:
                         return 2
                 
                 # x Button
                 if is_hit_box(mouse_pos,self.x_button[0], self.x_button[1]):
-                    print ("x")
+                    #print ("x")
                     if clickdown:
                         print ("comming soon...")
 
                 # x Button
                 if is_hit_box(mouse_pos,self.y_button[0], self.y_button[1]):
-                    print ("y")
+                    #print ("y")
                     if clickdown:
                         print ("comming soon...")
 
                 # back Button (main menu)
                 if is_hit_box(mouse_pos,self.back_button[0], self.back_button[1]):
-                    print ("back")
+                    #print ("back")
                     if clickdown:
                         return 0
 
@@ -226,21 +244,22 @@ def main():
     go = 0
     while go != -1:
         if go == 0:
-
             # open main music theme (ต้อง load ใหม่ เพื่อเปิดเสียงแยกจาก effect)
             pygame.mixer.music.load(join('resource','main_theme.wav'))
             pygame.mixer.music.set_volume(0.25)
             pygame.mixer.music.play(loops=-1)
-            
 
             go = menu.main()
+
         elif go == 1:
             go = menu.selection()
         elif go == 2:
-
             # stop main music theme
-            pygame.mixer.music.stop
+            pygame.mixer.music.stop()
+
             go = menu.pok()
+        elif go == 32:
+            go = menu.credit()
 
 
 
